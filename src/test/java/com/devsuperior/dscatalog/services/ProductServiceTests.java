d3e37f48b6fb83e7b864aaa1c302c058d77c9fc2
Roberto Okamura <rob.okamura@gmail.com>
2022-07-29 19:18:56 -0300

    Mockito adding behavior and delete existingId/nonExistingId

--- src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,11 +1,17 @@
 package com.devsuperior.dscatalog.services;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 //Usar quando a classe de teste não carrega o contexto da aplicação. 
 //É mais rápido e enxuto. Utiliza-se o @Mock
@@ -15,7 +21,37 @@ public class ProductServiceTests {
 	@InjectMocks
 	private ProductService service;
 	
-	@Mock
+	@Mock //configurar o comportamento simulado
 	private ProductRepository repository;
 	
+	private long existingId;
+	private long nonExistingId;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 1000L;
+		
+		Mockito.doNothing().when(repository).deleteById(existingId);
+		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);;
+	}
+	
+	@Test
+	public void deleteShouldDoNothingWhenIdExists() {
+		Assertions.assertDoesNotThrow(()->{
+			service.delete(existingId);
+		});
+		
+		Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+	}
+	
+	@Test
+	public void deleteShouldDoThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+		
+		Mockito.verify(repository, Mockito.times(1)).deleteById(nonExistingId);
+	}
+
 }
