61af7cf77ec3074559c0e78f282744c109754a40
Roberto Okamura <rob.okamura@gmail.com>
2022-07-30 16:55:08 -0300

    ProductControllerTests update, ArgumentMatchers.any

--- src/test/java/com/devsuperior/dscatalog/controllers/ProductControllerTests.java
+++ src/test/java/com/devsuperior/dscatalog/controllers/ProductControllerTests.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dscatalog.controllers;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,6 +24,7 @@ import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.services.ProductService;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscatalog.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 //Carrega o contexto, porém somente da camada web (teste de unidade: controlador)
 @WebMvcTest(ProductController.class)
@@ -30,8 +33,11 @@ public class ProductControllerTests {
 	@Autowired
 	private MockMvc mockMvc;
 	
-	@MockBean //Usar quando a classe de teste carrega o contexto da aplicação e precisa mockar algum bean do sistema.
+	@MockBean //Usar quando a classe de teste carrega o contexto da aplicação e precisa mockar algum bean do sistema
 	private ProductService service;
+	
+	@Autowired
+	private ObjectMapper objectMapper; //para converter objto Java para JSON
 	
 	private long existingId;
 	private long nonExistingId;
@@ -51,6 +57,10 @@ public class ProductControllerTests {
 		
 		when(service.findById(existingId)).thenReturn(productDTO);
 		when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+		
+		//ArgumentMatchers.eq pois utilizando o any() os outros objetos não podem ser objetos simples
+		when(service.update(eq(existingId), any())).thenReturn(productDTO);
+		when(service.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
 		
 	}
 	
@@ -79,6 +89,33 @@ public class ProductControllerTests {
 	@Test
 	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
 		mockMvc.perform(get("/products/{id}", nonExistingId)
+			.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void updateShouldReturnProductWhenIdExists() throws Exception {
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		
+		mockMvc.perform(put("/products/{id}", existingId)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.id").exists())
+		.andExpect(jsonPath("$.description").exists())
+		.andExpect(jsonPath("$.price").exists())
+		.andExpect(jsonPath("$.imgUrl").exists())
+		.andExpect(jsonPath("$.moment").exists());
+	}
+	
+	@Test
+	public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		
+		mockMvc.perform(put("/products/{id}", nonExistingId)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
 			.accept(MediaType.APPLICATION_JSON))
 		.andExpect(status().isNotFound());
 	}
