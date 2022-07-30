d086996eaa2853f5fa7c433f9e17e500438a4683
Roberto Okamura <rob.okamura@gmail.com>
2022-07-30 17:26:51 -0300

    Task done. ProductControllerTests, insert and delete

--- src/test/java/com/devsuperior/dscatalog/controllers/ProductControllerTests.java
+++ src/test/java/com/devsuperior/dscatalog/controllers/ProductControllerTests.java
@@ -2,8 +2,12 @@ package com.devsuperior.dscatalog.controllers;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,6 +26,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.services.ProductService;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscatalog.tests.Factory;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -41,6 +46,7 @@ public class ProductControllerTests {
 	
 	private long existingId;
 	private long nonExistingId;
+	private long dependentId;
 	
 	private ProductDTO productDTO;
 	private PageImpl<ProductDTO> page;
@@ -49,6 +55,7 @@ public class ProductControllerTests {
 	void setUp() throws Exception {
 		existingId = 1L;
 		nonExistingId = 2L;
+		dependentId = 3L;
 		
 		productDTO = Factory.createProductDTO();		
 		page = new PageImpl<>(List.of(productDTO));
@@ -58,9 +65,15 @@ public class ProductControllerTests {
 		when(service.findById(existingId)).thenReturn(productDTO);
 		when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
 		
+		when(service.insert(any())).thenReturn(productDTO);
+		
 		//ArgumentMatchers.eq pois utilizando o any() os outros objetos não podem ser objetos simples
 		when(service.update(eq(existingId), any())).thenReturn(productDTO);
 		when(service.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
+		
+		doNothing().when(service).delete(existingId);
+		doThrow(ResourceNotFoundException.class).when(service).delete(nonExistingId);
+		doThrow(DatabaseException.class).when(service).delete(dependentId);
 		
 	}
 	
@@ -94,6 +107,21 @@ public class ProductControllerTests {
 	}
 	
 	@Test
+	public void insertShouldReturnProductDTOCreated() throws Exception {
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		
+		mockMvc.perform(post("/products")
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isCreated())
+		.andExpect(jsonPath("$.description").exists())
+		.andExpect(jsonPath("$.price").exists())
+		.andExpect(jsonPath("$.imgUrl").exists())
+		.andExpect(jsonPath("$.moment").exists());
+	}
+	
+	@Test
 	public void updateShouldReturnProductWhenIdExists() throws Exception {
 		String jsonBody = objectMapper.writeValueAsString(productDTO);
 		
@@ -119,5 +147,20 @@ public class ProductControllerTests {
 			.accept(MediaType.APPLICATION_JSON))
 		.andExpect(status().isNotFound());
 	}
+	
+	@Test
+	public void deleteShouldReturnNoContentWhenIdExists() throws Exception{
+		mockMvc.perform(delete("/products/{id}", existingId)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNoContent()); 
+	}
+	
+	@Test
+	public void deleteShouldReturnNotFoundWhenIdDoesNotExist() throws Exception{
+		mockMvc.perform(delete("/products/{id}", nonExistingId)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNotFound());
+	}
+	
 	
 }
